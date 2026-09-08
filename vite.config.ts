28718ed9d87ad4a38aff48a8e5d7b543fecf3f09
krishnabiradar45 <24ug1bycs696@bmsit.in>
2026-09-08 15:07:19 +0530

    Use aitravel GitHub Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: process.env.GITHUB_PAGES ? '/travel/' : process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
+    base: process.env.GITHUB_PAGES ? '/aitravel/' : process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
